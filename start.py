8128f535c75f656af52c0b44aaf8d54bab4869e3
suhani khurana <suhani24khurana@gmail.com>
2018-06-18 22:30:42 +0530

    initial commit

---
+++ start.py
@@ -0,0 +1,11 @@
+import nltk
+
+fileLines = open('abc.txt', 'r').readlines()
+tokens = []
+i = 0
+
+while i < len(fileLines):
+    tokens.append(nltk.word_tokenize(fileLines[i]))
+    i += 1
+
+print(tokens)
